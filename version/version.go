101514aa4a80aa7dc98571a996a5f96dc5334362
Keisuke Isono <keisuke@reirou.jp>
2017-01-08 15:12:48 +0900

    Add version

---
+++ version/version.go
@@ -0,0 +1,30 @@
+package version
+
+import (
+  "fmt"
+)
+
+type Version struct {
+  Major int
+  Minor int
+  Patch int
+  PreReleaseVersionIndentity string
+  BuildMetadata string
+}
+
+var CmdVersion = Version{
+  Major: 0,
+  Minor: 1,
+  Patch: 0,
+}
+
+func(v Version) String() string {
+  versionString := fmt.Sprintf("%d.%d.%d", v.Major, v.Minor, v.Patch)
+  if len(v.PreReleaseVersionIndentity) > 0 {
+    versionString = fmt.Sprintf("%s-%s", versionString, v.PreReleaseVersionIndentity)
+  }
+  if len(v.BuildMetadata) > 0 {
+    versionString = fmt.Sprintf("%s+%s", versionString, v.BuildMetadata)
+  }
+  return versionString
+}
